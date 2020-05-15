5185608c6489b745bae8721910962bb3fe1624cc
Gede Lumbung <gedesumawijaya@gmail.com>
2020-05-15 18:11:23 +0800

    :fire: initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container mx-auto">
+      <div class="bg-white p-12 my-10 relative">
+        <div
+          class="icon bg-blue-600 text-white w-6 h-6 absolute flex items-center justify-center p-5"
+          style="left:-40px"
+        >
+          <i
+            class="fal fa-phone-volume fa-fw text-2xl transform -rotate-45"
+          ></i>
+        </div>
+        <h3 class="text-2xl text-gray-900 font-semibold">
+          CURL Command Builder
+        </h3>
+        <p class="text-gray-600">Simple thing, but this we always forget :p</p>
+
+        <input
+          type="tel"
+          name=""
+          id=""
+          placeholder="URL"
+          class="border p-2 w-full space-x-5 mt-5"
+        />
+        <select class="block appearance-none w-full bg-white border p-2 w-full mt-3">
+          <option>-- HTTP METHOD --</option>
+          <option>GET</option>
+          <option>POST</option>
+          <option>PUT</option>
+          <option>DELETE</option>
+          <option>PATCH</option>
+        </select>
+        <textarea
+          name=""
+          id=""
+          cols="10"
+          rows="3"
+          placeholder="Tell us about desired property"
+          class="border p-2 mt-3 w-full"
+        ></textarea>
+      </div>
+    </div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
